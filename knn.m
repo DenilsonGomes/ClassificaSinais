ee820c9683d95ad6b4c07d69a619382330ffcbcb
Denilson Gomes <denilsongomesdg97@gmail.com>
2019-09-20 13:40:37 -0300

    Codigo do hnn iniciado

---
+++ knn.m
@@ -0,0 +1,9 @@
+%Autor: Denilson Gomes Vaz da Silva <denilsongomes@alu.ufc.br>
+%Graduando em Engenharia da Computação
+%Reconhecimento de padroes
+
+%Algoritmo para classificar base de dados usando o K-NN
+
+%Carrega base de dados
+load('Classe1.mat')
+load('Classe2.mat')
