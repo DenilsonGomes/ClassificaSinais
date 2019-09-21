c2cae1cc27076afd3d5c731ca4cfb0aefed64f01
Denilson Gomes <denilsongomesdg97@gmail.com>
2019-09-20 23:52:58 -0300

    arquivo do KNN finalizado, implementado leave-one-out.

--- ClassificaComKNN.m
+++ ClassificaComKNN.m
@@ -100,9 +100,10 @@ dados(51:100,8) = MedianaFurrier2(51:end);
 classes(1:50) = dataset(1:50,end);
 classes(51:100) = dataset(51:end,end);
 
-%k-Fold
-kfold=10;
+%k-Fold (para k=5 e k=10)
+kfold=5;
 kvizinhos = 10;
+disp('Matriz confusão da classificação do KNN com K-Fold')
 for i=1:kfold
     [~,numAtributos] = size(dados);
     acertos1 = 0;
@@ -163,3 +164,61 @@ for i=1:kfold
     %Elemento 22 mostra o numero de vez que o algoritmo disse ser a classe 2, e de fato era a classe 2.
     [acertos1 erros1; erros2 acertos2]
 end
+
+%leave-one-out
+kvizinhos = 10;
+acertos1 = 0;
+erros1 = 0;
+acertos2 = 0;
+erros2 = 0;
+disp('Matriz confusão da classificação do Nearest Prototype com leave-on-out')
+for t=1:length(dados) %asmostras (Testes)
+    dadosAux = dados;
+    classeAux = classes;
+    
+    %pega a amostra de teste
+    Teste = dadosAux(t,:);
+    classeTeste = classes(t);
+         
+    %pega as amostras de treino
+    dadosAux(t,:)=[];
+    classeAux(t)=[];
+    for j=1:length(dadosAux) %asmostras (Treinamento)
+        d=0; %reseta d para a distancia de um novo vetor
+        for p=1:numAtributos %para todos os atributos das amostras
+            %d é a soma dos quadrados das diferencas das coordenadas da amostra i para a amostra j
+            d = d + (Teste(p) - dadosAux(j,p))^2; 
+        end
+        %dist é a raiz quadrada da soma dos quadrados das coordenadas (distancia euclidiana)
+        dist(j) = sqrt(d);
+     end
+        
+     for m=1:kvizinhos %para os k-vizinhos mais perto da amostra t
+        [~,indice] = min(dist); %pegamos o indice do vizinho mais proximo
+        knn(m) = classeAux(indice); %colocamos a classe desta amostra em knn
+        dist(indice) = []; %removemos esta amostra
+        dadosAux(indice,:) = []; %removemos esta amostra
+        classeAux(indice) = []; %removemos a classe da amostra retirada (questao de dimensionamento)
+     end
+     %verificamos se a classe que ocorre em maior numero eh a classe
+     %real do teste
+     if(mode(knn) == classeTeste && classeTeste == 1) %caso acerte um teste da classe real 1
+        acertos1 = acertos1 + 1; %acrescentamos acertos1
+     end
+     if(mode(knn) == classeTeste && classeTeste == 2) %caso acerte um teste da classe real 2
+        acertos2 = acertos2 + 1; %acrescentamos acertos2
+     end
+     if(mode(knn) ~= classeTeste &&classeTeste == 1) %caso erre um teste da classe real 1
+        erros1 = erros1 + 1; %acrescentamos erros1
+     end
+     if(mode(knn) ~= classeTeste && classeTeste == 2) %caso erre um teste da classe real 2
+        erros2 = erros2 + 1; %acrescentamos erros2
+     end
+
+    %Matriz de confusão: 
+    %Elemento 11 mostra o numero de vez que o algoritmo disse ser a classe 1, e de fato era a classe 1.
+    %Elemento 12 mostra o numero de vez que o algoritmo disse ser a classe 2, e era a classe 1.
+    %Elemento 21 mostra o numero de vez que o algoritmo disse ser a classe 1, e era a classe 2.
+    %Elemento 22 mostra o numero de vez que o algoritmo disse ser a classe 2, e de fato era a classe 2.
+end
+[acertos1 erros1; erros2 acertos2]
